a536482dda7ec27231c5b688c416ccdd1d043b83
Ebubekir42 <ebubekirturkk2001@gmail.com>
2026-01-10 15:25:29 +0300

    Array

---
+++ src/Array.ts
@@ -0,0 +1,11 @@
+let names: string[] = [];
+names.push("Dylan");
+// names.push(3);
+
+let names2: readonly string[] = ["Dylan"];
+// names2.push("Jack");
+
+let numbers = [1, 2, 3];
+numbers.push(4);
+// numbers.push("2");
+let head: number = numbers[0] ?? 0;
